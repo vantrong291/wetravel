b068ea22856cd2336985758aeecead25314de24a
vantrong291 <vantrong291@gmail.com>
2019-09-24 20:00:31 +0700

    update details

--- App/Screens/TourDetail/index.js
+++ App/Screens/TourDetail/index.js
@@ -13,6 +13,8 @@ import Constants from '../../Theme/Constants'
 import RateComponent from '../../Components/RateComponent'
 import SectionedMultiSelect from 'react-native-sectioned-multi-select'
 import SelectInput from '../../Components/ReuseComponents/SelectInput'
+import SlidingUpPanel from 'rn-sliding-up-panel';
+import BottomSheet from '../../Components/BottomSheet'
 
 const items = [
   {
@@ -62,7 +64,7 @@ class TourDetail extends React.Component {
 
   componentDidMount() {
     runAfter(() => this.setState({ loading: false }), 200)
-  }
+  };
 
   goBack = () => {
     this.props.navigation.goBack()
@@ -98,7 +100,7 @@ class TourDetail extends React.Component {
               {tour.introduce}
             </Text>
           </View>
-          <View style={{flexDirection: 'row'}}>
+          <BottomSheet children={<View style={{flexDirection: 'row'}}>
             <SelectInput
               items={items}
               uniqueKey="id"
@@ -123,7 +125,7 @@ class TourDetail extends React.Component {
               selectedItems={this.state.selectedItems}
               containerStyle={{width: '50%'}}
             />
-          </View>
+          </View>}/>
         </ScrollView>
         <ActionButton
           buttonColor={Colors.navbarColor}
@@ -144,3 +146,140 @@ class TourDetail extends React.Component {
 }
 
 export default TourDetail
+
+// import React from "react";
+// import { Text, View, Dimensions, Animated } from "react-native";
+//
+// import SlidingUpPanel from "rn-sliding-up-panel";
+//
+// const { height } = Dimensions.get("window");
+//
+// const styles = {
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#f8f9fa",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   },
+//   panel: {
+//     flex: 1,
+//     backgroundColor: "white",
+//     position: "relative"
+//   },
+//   panelHeader: {
+//     height: 180,
+//     backgroundColor: "#b197fc",
+//     justifyContent: "flex-end",
+//     padding: 24
+//   },
+//   textHeader: {
+//     fontSize: 28,
+//     color: "#FFF"
+//   },
+//   icon: {
+//     alignItems: "center",
+//     justifyContent: "center",
+//     position: "absolute",
+//     top: -24,
+//     right: 18,
+//     width: 48,
+//     height: 48,
+//     zIndex: 1
+//   },
+//   iconBg: {
+//     backgroundColor: "#2b8a3e",
+//     position: "absolute",
+//     top: -24,
+//     right: 18,
+//     width: 48,
+//     height: 48,
+//     borderRadius: 24,
+//     zIndex: 1
+//   }
+// };
+//
+// class TourDetail extends React.Component {
+//   static defaultProps = {
+//     draggableRange: { top: height + 180 - 64, bottom: 180 }
+//   };
+//
+//   _draggedValue = new Animated.Value(180);
+//
+//   render() {
+//     const { top, bottom } = this.props.draggableRange;
+//
+//     const backgoundOpacity = this._draggedValue.interpolate({
+//       inputRange: [height - 48, height],
+//       outputRange: [1, 0],
+//       extrapolate: "clamp"
+//     });
+//
+//     const iconTranslateY = this._draggedValue.interpolate({
+//       inputRange: [height - 56, height, top],
+//       outputRange: [0, 56, 180 - 32],
+//       extrapolate: "clamp"
+//     });
+//
+//     const textTranslateY = this._draggedValue.interpolate({
+//       inputRange: [bottom, top],
+//       outputRange: [0, 8],
+//       extrapolate: "clamp"
+//     });
+//
+//     const textTranslateX = this._draggedValue.interpolate({
+//       inputRange: [bottom, top],
+//       outputRange: [0, -112],
+//       extrapolate: "clamp"
+//     });
+//
+//     const textScale = this._draggedValue.interpolate({
+//       inputRange: [bottom, top],
+//       outputRange: [1, 0.7],
+//       extrapolate: "clamp"
+//     });
+//
+//     return (
+//       <View style={styles.container}>
+//         <Text onPress={() => this._panel.show(360)}>Show panel</Text>
+//         <SlidingUpPanel
+//           ref={c => (this._panel = c)}
+//           draggableRange={this.props.draggableRange}
+//           animatedValue={this._draggedValue}
+//           snappingPoints={[360]}
+//           height={height + 180}
+//           friction={0.5}
+//         >
+//           <View style={styles.panel}>
+//             <Animated.View
+//               style={[
+//                 styles.iconBg,
+//                 {
+//                   opacity: backgoundOpacity,
+//                   transform: [{ translateY: iconTranslateY }]
+//                 }
+//               ]}
+//             />
+//             <View style={styles.panelHeader}>
+//               <Animated.View
+//                 style={{
+//                   transform: [
+//                     { translateY: textTranslateY },
+//                     { translateX: textTranslateX },
+//                     { scale: textScale }
+//                   ]
+//                 }}
+//               >
+//                 <Text style={styles.textHeader}>Sliding Up Panel</Text>
+//               </Animated.View>
+//             </View>
+//             <View style={styles.container}>
+//               <Text>Bottom sheet content</Text>
+//             </View>
+//           </View>
+//         </SlidingUpPanel>
+//       </View>
+//     );
+//   }
+// }
+//
+// export default TourDetail;
